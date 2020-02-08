45fee7aac114f684593022a7fe3de18e8ec294e1
Edward Williams <eddie_w38@yahoo.com>
2020-02-08 17:43:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,4 +9,7 @@ hash["new_key"] << "new_value_for_value_array"
  
 hash
  => {"new_key"=>["new_value_for_value_array"]} 
- 
+ school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
